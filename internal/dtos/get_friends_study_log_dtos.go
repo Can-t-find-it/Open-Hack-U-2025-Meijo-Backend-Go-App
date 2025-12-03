f395062c32a3b53486aae1d175d06de5f1b2813e
Yuki Ueno <145101888+Rikishi-com@users.noreply.github.com>
2025-12-03 16:09:20 +0900

    Reapply "Reapply "fix json for get study log""
    This reverts commit 1aa8eee1da445a445815c71dbabf077ac944e2cd.

--- internal/dtos/get_friends_study_log_dtos.go
+++ internal/dtos/get_friends_study_log_dtos.go
@@ -2,14 +2,24 @@ package dtos
 
 import "time"
 
-type InputFriendsStudyLog struct {
-	FriendsID []uint `json:"friends_id"`
+// type InputFriendsStudyLog struct {
+// 	FriendsID []uint `json:"friends_id"`
+// }
+
+type FriendStudyLog struct {
+	ID            uint      `json:"logId"`
+	FriendID      uint      `json:"friendId"`
+	FriendName    string    `json:"friendName"` //新規追加
+	AnsweredAt    time.Time `json:"dateTime"`
+	TextbookName  string    `json:"textbookName"`  //新規追加
+	Accuracy      float64   `json:"accuracy"`      //新規追加
+	TodayProgress uint      `json:"todayProgress"` //新規追加
+
+	// QuestionID uint      `json:"question_id"`
+	// Answered   bool      `json:"answered"`
+	// Score      int       `json:"score"`
 }
 
-type ResponseFriendStudyLog struct {
-	FriendID   uint      `json:"friend_id"`
-	QuestionID uint      `json:"question_id"`
-	Answered   bool      `json:"answered"`
-	AnsweredAt time.Time `json:"answered_at"`
-	Score      int       `json:"score"`
+type AllFriendsStudyLog struct {
+	Logs []FriendStudyLog `json:"logs"`
 }
